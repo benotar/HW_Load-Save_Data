e3239b2d79443a2d8654a1a208529e3d5899ba3f
Ivan <zhmur.ivan@gmail.com>
2023-12-26 13:17:16 +0200

    Add class User

---
+++ Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using HL_26_12_2023.Abstractions;
+
+namespace HL_26_12_2023.Entities;
+
+public class User : Entity
+{
+    public string UserName { get; set; }
+    public string? RealName { get; set; }
+    public override string ToString() =>
+        $"User {UserName} ({RealName ?? "N/A"})";
+}
